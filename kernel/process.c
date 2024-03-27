f6dbc09080a2e9c1d2b4414fc6a256b17ea89843
ssk015 <18252677381@163.com>
2024-03-27 17:15:17 +0800

    feat: finish lab4_c2

--- kernel/process.c
+++ kernel/process.c
@@ -1,30 +1,30 @@
 /*
  * Utility functions for process management.
  *
- * Note: in Lab1, only one process (i.e., our user application) exists.
- * Therefore, PKE OS at this stage will set "current" to the loaded user
- * application, and also switch to the old "current" process after trap
- * handling.
+ * Note: in Lab1, only one process (i.e., our user application) exists. Therefore,
+ * PKE OS at this stage will set "current" to the loaded user application, and also
+ * switch to the old "current" process after trap handling.
  */
 
-#include "process.h"
-#include "config.h"
-#include "elf.h"
-#include "memlayout.h"
-#include "pmm.h"
 #include "riscv.h"
-#include "sched.h"
-#include "spike_interface/spike_utils.h"
 #include "strap.h"
+#include "config.h"
+#include "process.h"
+#include "elf.h"
 #include "string.h"
 #include "vmm.h"
+#include "pmm.h"
+#include "memlayout.h"
+#include "sched.h"
+#include "spike_interface/spike_utils.h"
+
 
 // Two functions defined in kernel/usertrap.S
 extern char smode_trap_vector[];
 extern void return_to_user(trapframe *, uint64 satp);
 
-// trap_sec_start points to the beginning of S-mode trap segment (i.e., the
-// entry point of S-mode trap vector).
+// trap_sec_start points to the beginning of S-mode trap segment (i.e., the entry point
+// of S-mode trap vector).
 extern char trap_sec_start[];
 
 // process pool. added @lab3_1
@@ -33,58 +33,54 @@ process procs[NPROC];
 // current points to the currently running user-mode application.
 process *current = NULL;
 
-// points to the first free page in our simple heap. added @lab2_2
-uint64 g_ufree_page = USER_FREE_ADDRESS_START;
-
 //
 // switch to a user-mode process
 //
-void switch_to(process *proc) {
+void switch_to(process *proc)
+{
   assert(proc);
   current = proc;
 
-  // write the smode_trap_vector (64-bit func. address) defined in
-  // kernel/strap_vector.S to the stvec privilege register, such that trap
-  // handler pointed by smode_trap_vector will be triggered when an interrupt
-  // occurs in S mode.
+  // write the smode_trap_vector (64-bit func. address) defined in kernel/strap_vector.S
+  // to the stvec privilege register, such that trap handler pointed by smode_trap_vector
+  // will be triggered when an interrupt occurs in S mode.
   write_csr(stvec, (uint64)smode_trap_vector);
 
-  // set up trapframe values (in process structure) that smode_trap_vector will
-  // need when the process next re-enters the kernel.
+  // set up trapframe values (in process structure) that smode_trap_vector will need when
+  // the process next re-enters the kernel.
   proc->trapframe->kernel_sp = proc->kstack;     // process's kernel stack
   proc->trapframe->kernel_satp = read_csr(satp); // kernel page table
   proc->trapframe->kernel_trap = (uint64)smode_trap_handler;
 
   // SSTATUS_SPP and SSTATUS_SPIE are defined in kernel/riscv.h
-  // set S Previous Privilege mode (the SSTATUS_SPP bit in sstatus register) to
-  // User mode.
+  // set S Previous Privilege mode (the SSTATUS_SPP bit in sstatus register) to User mode.
   unsigned long x = read_csr(sstatus);
   x &= ~SSTATUS_SPP; // clear SPP to 0 for user mode
   x |= SSTATUS_SPIE; // enable interrupts in user mode
 
-  // write x back to 'sstatus' register to enable interrupts, and sret
-  // destination mode.
+  // write x back to 'sstatus' register to enable interrupts, and sret destination mode.
   write_csr(sstatus, x);
 
   // set S Exception Program Counter (sepc register) to the elf entry pc.
   write_csr(sepc, proc->trapframe->epc);
 
-  // make user page table. macro MAKE_SATP is defined in kernel/riscv.h. added
-  // @lab2_1
+  // make user page table. macro MAKE_SATP is defined in kernel/riscv.h. added @lab2_1
   uint64 user_satp = MAKE_SATP(proc->pagetable);
 
-  // return_to_user() is defined in kernel/strap_vector.S. switch to user mode
-  // with sret. note, return_to_user takes two parameters @ and after lab2_1.
+  // return_to_user() is defined in kernel/strap_vector.S. switch to user mode with sret.
+  // note, return_to_user takes two parameters @ and after lab2_1.
   return_to_user(proc->trapframe, user_satp);
 }
 
 //
 // initialize process pool (the procs[] array). added @lab3_1
 //
-void init_proc_pool() {
+void init_proc_pool()
+{
   memset(procs, 0, sizeof(process) * NPROC);
 
-  for (int i = 0; i < NPROC; ++i) {
+  for (int i = 0; i < NPROC; ++i)
+  {
     procs[i].status = FREE;
     procs[i].pid = i;
   }
@@ -94,7 +90,8 @@ void init_proc_pool() {
 // allocate an empty process, init its vm space. returns the pointer to
 // process strcuture. added @lab3_1
 //
-process *alloc_process() {
+process *alloc_process()
+{
   // locate the first usable process structure
   int i;
 
@@ -102,14 +99,14 @@ process *alloc_process() {
     if (procs[i].status == FREE)
       break;
 
-  if (i >= NPROC) {
+  if (i >= NPROC)
+  {
     panic("cannot find any free process structure.\n");
     return 0;
   }
 
   // init proc[i]'s vm space
-  procs[i].trapframe =
-      (trapframe *)alloc_page(); // trapframe, used to save context
+  procs[i].trapframe = (trapframe *)alloc_page(); // trapframe, used to save context
   memset(procs[i].trapframe, 0, sizeof(trapframe));
 
   // page directory
@@ -117,10 +114,8 @@ process *alloc_process() {
   memset((void *)procs[i].pagetable, 0, PGSIZE);
 
   procs[i].kstack = (uint64)alloc_page() + PGSIZE; // user kernel stack top
-  uint64 user_stack =
-      (uint64)alloc_page(); // phisical address of user stack bottom
-  procs[i].trapframe->regs.sp =
-      USER_STACK_TOP; // virtual address of user stack top
+  uint64 user_stack = (uint64)alloc_page();        // phisical address of user stack bottom
+  procs[i].trapframe->regs.sp = USER_STACK_TOP;    // virtual address of user stack top
 
   // allocates a page to record memory regions (segments)
   procs[i].mapped_info = (mapped_region *)alloc_page();
@@ -129,35 +124,43 @@ process *alloc_process() {
   // map user stack in userspace
   user_vm_map((pagetable_t)procs[i].pagetable, USER_STACK_TOP - PGSIZE, PGSIZE,
               user_stack, prot_to_type(PROT_WRITE | PROT_READ, 1));
-  procs[i].mapped_info[0].va = USER_STACK_TOP - PGSIZE;
-  procs[i].mapped_info[0].npages = 1;
-  procs[i].mapped_info[0].seg_type = STACK_SEGMENT;
+  procs[i].mapped_info[STACK_SEGMENT].va = USER_STACK_TOP - PGSIZE;
+  procs[i].mapped_info[STACK_SEGMENT].npages = 1;
+  procs[i].mapped_info[STACK_SEGMENT].seg_type = STACK_SEGMENT;
 
   // map trapframe in user space (direct mapping as in kernel space).
-  user_vm_map((pagetable_t)procs[i].pagetable, (uint64)procs[i].trapframe,
-              PGSIZE, (uint64)procs[i].trapframe,
-              prot_to_type(PROT_WRITE | PROT_READ, 0));
-  procs[i].mapped_info[1].va = (uint64)procs[i].trapframe;
-  procs[i].mapped_info[1].npages = 1;
-  procs[i].mapped_info[1].seg_type = CONTEXT_SEGMENT;
+  user_vm_map((pagetable_t)procs[i].pagetable, (uint64)procs[i].trapframe, PGSIZE,
+              (uint64)procs[i].trapframe, prot_to_type(PROT_WRITE | PROT_READ, 0));
+  procs[i].mapped_info[CONTEXT_SEGMENT].va = (uint64)procs[i].trapframe;
+  procs[i].mapped_info[CONTEXT_SEGMENT].npages = 1;
+  procs[i].mapped_info[CONTEXT_SEGMENT].seg_type = CONTEXT_SEGMENT;
 
-  // map S-mode trap vector section in user space (direct mapping as in kernel
-  // space) we assume that the size of usertrap.S is smaller than a page.
+  // map S-mode trap vector section in user space (direct mapping as in kernel space)
+  // we assume that the size of usertrap.S is smaller than a page.
   user_vm_map((pagetable_t)procs[i].pagetable, (uint64)trap_sec_start, PGSIZE,
               (uint64)trap_sec_start, prot_to_type(PROT_READ | PROT_EXEC, 0));
-  procs[i].mapped_info[2].va = (uint64)trap_sec_start;
-  procs[i].mapped_info[2].npages = 1;
-  procs[i].mapped_info[2].seg_type = SYSTEM_SEGMENT;
+  procs[i].mapped_info[SYSTEM_SEGMENT].va = (uint64)trap_sec_start;
+  procs[i].mapped_info[SYSTEM_SEGMENT].npages = 1;
+  procs[i].mapped_info[SYSTEM_SEGMENT].seg_type = SYSTEM_SEGMENT;
 
-  sprint(
-      "in alloc_proc. user frame 0x%lx, user stack 0x%lx, user kstack 0x%lx \n",
-      procs[i].trapframe, procs[i].trapframe->regs.sp, procs[i].kstack);
+  sprint("in alloc_proc. user frame 0x%lx, user stack 0x%lx, user kstack 0x%lx \n",
+         procs[i].trapframe, procs[i].trapframe->regs.sp, procs[i].kstack);
+
+  // initialize the process's heap manager
+  procs[i].user_heap.heap_top = USER_FREE_ADDRESS_START;
+  procs[i].user_heap.heap_bottom = USER_FREE_ADDRESS_START;
+  procs[i].user_heap.free_pages_count = 0;
+
+  // map user heap in userspace
+  procs[i].mapped_info[HEAP_SEGMENT].va = USER_FREE_ADDRESS_START;
+  procs[i].mapped_info[HEAP_SEGMENT].npages = 0; // no pages are mapped to heap yet.
+  procs[i].mapped_info[HEAP_SEGMENT].seg_type = HEAP_SEGMENT;
+
+  procs[i].total_mapped_region = 4;
 
   // initialize files_struct
   procs[i].pfiles = init_proc_file_management();
   sprint("in alloc_proc. build proc_file_management successfully.\n");
-
-  procs[i].total_mapped_region = 3;
 
   // return after initialization.
   return &procs[i];
@@ -166,11 +169,12 @@ process *alloc_process() {
 //
 // reclaim a process. added @lab3_1
 //
-int free_process(process *proc) {
+int free_process(process *proc)
+{
   // we set the status to ZOMBIE, but cannot destruct its vm space immediately.
-  // since proc can be current process, and its user kernel stack is currently
-  // in use! but for proxy kernel, it (memory leaking) may NOT be a really
-  // serious issue, as it is different from regular OS, which needs to run 7x24.
+  // since proc can be current process, and its user kernel stack is currently in use!
+  // but for proxy kernel, it (memory leaking) may NOT be a really serious issue,
+  // as it is different from regular OS, which needs to run 7x24.
   proc->status = ZOMBIE;
 
   return 0;
@@ -178,51 +182,105 @@ int free_process(process *proc) {
 
 //
 // implements fork syscal in kernel. added @lab3_1
-// basic idea here is to first allocate an empty process (child), then duplicate
-// the context and data segments of parent process to the child, and lastly, map
-// other segments (code, system) of the parent to child. the stack segment
-// remains unchanged for the child.
+// basic idea here is to first allocate an empty process (child), then duplicate the
+// context and data segments of parent process to the child, and lastly, map other
+// segments (code, system) of the parent to child. the stack segment remains unchanged
+// for the child.
 //
-int do_fork(process *parent) {
+int do_fork(process *parent)
+{
   sprint("will fork a child from parent %d.\n", parent->pid);
   process *child = alloc_process();
 
-  for (int i = 0; i < parent->total_mapped_region; i++) {
+  for (int i = 0; i < parent->total_mapped_region; i++)
+  {
     // browse parent's vm space, and copy its trapframe and data segments,
     // map its code segment.
-    switch (parent->mapped_info[i].seg_type) {
+    switch (parent->mapped_info[i].seg_type)
+    {
     case CONTEXT_SEGMENT:
+    {
       *child->trapframe = *parent->trapframe;
       break;
+    }
     case STACK_SEGMENT:
-      memcpy((void *)lookup_pa(child->pagetable, child->mapped_info[0].va),
-             (void *)lookup_pa(parent->pagetable, parent->mapped_info[i].va),
-             PGSIZE);
-      break;
-    case CODE_SEGMENT:
-      // TODO (lab3_1): implment the mapping of child code segment to parent's
-      // code segment.
-      // hint: the virtual address mapping of code segment is tracked in
-      // mapped_info page of parent's process structure. use the information in
-      // mapped_info to retrieve the virtual to physical mapping of code
-      // segment. after having the mapping information, just map the
-      // corresponding virtual address region of child to the physical pages
-      // that actually store the code segment of parent process. DO NOT COPY THE
-      // PHYSICAL PAGES, JUST MAP THEM.
-      map_pages(child->pagetable, parent->mapped_info[i].va,
-                parent->mapped_info[i].npages * PGSIZE,
-                lookup_pa(parent->pagetable, parent->mapped_info[i].va),
-                prot_to_type(PROT_EXEC | PROT_READ, 1));
-
-      // after mapping, register the vm region (do not delete codes below!)
-      child->mapped_info[child->total_mapped_region].va =
-          parent->mapped_info[i].va;
-      child->mapped_info[child->total_mapped_region].npages =
-          parent->mapped_info[i].npages;
-      child->mapped_info[child->total_mapped_region].seg_type = CODE_SEGMENT;
-      child->total_mapped_region++;
+    {
+      memcpy((void *)lookup_pa(child->pagetable, child->mapped_info[STACK_SEGMENT].va),
+             (void *)lookup_pa(parent->pagetable, parent->mapped_info[i].va), PGSIZE);
       break;
     }
+    case HEAP_SEGMENT:
+    {
+      // build a same heap for child process.
+
+      // convert free_pages_address into a filter to skip reclaimed blocks in the heap
+      // when mapping the heap blocks
+      int free_block_filter[MAX_HEAP_PAGES];
+      memset(free_block_filter, 0, MAX_HEAP_PAGES);
+      uint64 heap_bottom = parent->user_heap.heap_bottom;
+      for (int i = 0; i < parent->user_heap.free_pages_count; i++)
+      {
+        int index = (parent->user_heap.free_pages_address[i] - heap_bottom) / PGSIZE;
+        free_block_filter[index] = 1;
+      }
+
+      // copy and map the heap blocks
+      for (uint64 heap_block = current->user_heap.heap_bottom;
+           heap_block < current->user_heap.heap_top; heap_block += PGSIZE)
+      {
+        if (free_block_filter[(heap_block - heap_bottom) / PGSIZE]) // skip free blocks
+          continue;
+
+        void *child_pa = alloc_page();
+        memcpy(child_pa, (void *)lookup_pa(parent->pagetable, heap_block), PGSIZE);
+        user_vm_map((pagetable_t)child->pagetable, heap_block, PGSIZE, (uint64)child_pa,
+                    prot_to_type(PROT_WRITE | PROT_READ, 1));
+      }
+
+      child->mapped_info[HEAP_SEGMENT].npages = parent->mapped_info[HEAP_SEGMENT].npages;
+
+      // copy the heap manager from parent to child
+      memcpy((void *)&child->user_heap, (void *)&parent->user_heap, sizeof(parent->user_heap));
+      break;
+    }
+    case CODE_SEGMENT:
+    {
+      // TODO (lab3_1): implment the mapping of child code segment to parent's
+      // code segment.
+      // hint: the virtual address mapping of code segment is tracked in mapped_info
+      // page of parent's process structure. use the information in mapped_info to
+      // retrieve the virtual to physical mapping of code segment.
+      // after having the mapping information, just map the corresponding virtual
+      // address region of child to the physical pages that actually store the code
+      // segment of parent process.
+      // DO NOT COPY THE PHYSICAL PAGES, JUST MAP THEM.
+      // panic( "You need to implement the code segment mapping of child in lab3_1.\n" );
+
+      // user_vm_map((pagetable_t)child->pagetable,
+      //             parent->mapped_info[CODE_SEGMENT].va,
+      //             parent->mapped_info[CODE_SEGMENT].npages * PGSIZE,
+      //             lookup_pa(parent->pagetable, parent->mapped_info[CODE_SEGMENT].va),
+      //             prot_to_type(PROT_READ | PROT_EXEC, 1));
+      // sprint("do_folk map code segment at pa:0x%lx of parent to child at va:0x%lx.\n", lookup_pa(parent->pagetable, parent->mapped_info[CODE_SEGMENT].va), parent->mapped_info[CODE_SEGMENT].va);
+      // break;
+
+      for (int j = 0; j < parent->mapped_info[i].npages; j++)
+      {
+        uint64 addr = lookup_pa(parent->pagetable, parent->mapped_info[i].va + j * PGSIZE);
+
+        map_pages(child->pagetable, parent->mapped_info[i].va + j * PGSIZE, PGSIZE,
+                  addr, prot_to_type(PROT_WRITE | PROT_READ | PROT_EXEC, 1));
+
+        sprint("do_fork map code segment at pa:%lx of parent to child at va:%lx.\n",
+               addr, parent->mapped_info[i].va + j * PGSIZE);
+      }
+    }
+    }
+    // after mapping, register the vm region (do not delete codes below!)
+    child->mapped_info[child->total_mapped_region].va = parent->mapped_info[i].va;
+    child->mapped_info[child->total_mapped_region].npages = parent->mapped_info[i].npages;
+    child->mapped_info[child->total_mapped_region].seg_type = CODE_SEGMENT;
+    child->total_mapped_region++;
   }
 
   child->status = READY;
@@ -231,4 +289,37 @@ int do_fork(process *parent) {
   insert_to_ready_queue(child);
 
   return child->pid;
+}
+
+
+// added @lab4_c2
+// reclaim the open-file management data structure of a process.
+// exec会根据读入的可执行文件将'原进程'的数据段、代码段和堆栈段替换。
+int do_execv(char *path)
+{
+  spike_file_t * file = spike_file_open(path,O_RDONLY, 0);
+  if(IS_ERR_VALUE(file))
+  {
+    panic("Fail on openning the input application program.\n");
+    return -1;
+  }else{
+    sprint("file:%s open success\n",path);
+  }
+  elf_ctx elfloader;
+  elf_info info;
+  info.f = file;
+  info.p = current;
+  if(elf_init(&elfloader, &info) != EL_OK){
+    panic("fail to init elfloader.\n");
+    return -1;
+  }
+  sprint("elf_loader: phnum:%d\n", elfloader.ehdr.phnum);
+  if(elf_reload(&elfloader, &info) != EL_OK){
+    panic("Fail on loading elf.\n");
+    return -1;
+  }
+  current->trapframe->epc = elfloader.ehdr.entry;
+  spike_file_close(file);
+  sprint("Application program entry point (virtual address): 0x%lx\n", current->trapframe->epc);
+  return 0;
 }
--- kernel/strap.c
+++ kernel/strap.c
@@ -2,16 +2,13 @@
  * Utility functions for trap handling in Supervisor mode.
  */
 
-#include "strap.h"
-#include "pmm.h"
-#include "process.h"
 #include "riscv.h"
+#include "process.h"
+#include "strap.h"
 #include "syscall.h"
+#include "pmm.h"
 #include "vmm.h"
-
 #include "sched.h"
-// #include "memlayout.h"
-
 #include "util/functions.h"
 
 #include "spike_interface/spike_utils.h"
@@ -19,21 +16,17 @@
 //
 // handling the syscalls. will call do_syscall() defined in kernel/syscall.c
 //
-static void handle_syscall(trapframe *tf) {
-  // tf->epc points to the address that our computer will jump to after the trap
-  // handling. for a syscall, we should return to the NEXT instruction after its
-  // handling. in RV64G, each instruction occupies exactly 32 bits (i.e., 4
-  // Bytes)
+static void handle_syscall(trapframe *tf)
+{
+  // tf->epc points to the address that our computer will jump to after the trap handling.
+  // for a syscall, we should return to the NEXT instruction after its handling.
+  // in RV64G, each instruction occupies exactly 32 bits (i.e., 4 Bytes)
   tf->epc += 4;
-
+  tf->regs.a0 = do_syscall(tf->regs.a0, tf->regs.a1, tf->regs.a2, tf->regs.a3, tf->regs.a4, tf->regs.a5, tf->regs.a6, tf->regs.a7);
   // TODO (lab1_1): remove the panic call below, and call do_syscall (defined in
-  // kernel/syscall.c) to conduct real operations of the kernel side for a
-  // syscall. IMPORTANT: return value should be returned to user app, or else,
-  // you will encounter problems in later experiments! char* data = "Hello
-  // world!\n"; long data_ptr = (long)data; do_syscall(64,data_ptr,1,0,0,0,0,0);
-  // do_syscall(65,0,0,0,0,0,0,0);
-  tf->regs.a0 = do_syscall(tf->regs.a0, tf->regs.a1, tf->regs.a2, tf->regs.a3,
-                           tf->regs.a4, tf->regs.a5, tf->regs.a6, tf->regs.a7);
+  // kernel/syscall.c) to conduct real operations of the kernel side for a syscall.
+  // IMPORTANT: return value should be returned to user app, or else, you will encounter
+  // problems in later experiments!
   // panic( "call do_syscall to accomplish the syscall and lab1_1 here.\n" );
 }
 
@@ -43,19 +36,15 @@ static uint64 g_ticks = 0;
 //
 // added @lab1_3
 //
-void handle_mtimer_trap() {
+void handle_mtimer_trap()
+{
   sprint("Ticks %d\n", g_ticks);
-  // TODO (lab1_3): increase g_ticks to record this "tick", and then clear the
-  // "SIP"
-  g_ticks++;
-
+  // TODO (lab1_3): increase g_ticks to record this "tick", and then clear the "SIP"
   // field in sip register.
-  // write_csr
   // hint: use write_csr to disable the SIP_SSIP bit in sip.
-  int aaa = read_csr(sip);
-  write_csr(sip, ~(aaa & 2));
-  // panic( "lab1_3: increase g_ticks by one, and clear SIP field in sip
-  // register.\n" );
+  g_ticks++;
+  write_csr(sip, 0);
+  // panic( "lab1_3: increase g_ticks by one, and clear SIP field in sip register.\n" );
 }
 
 //
@@ -63,34 +52,22 @@ void handle_mtimer_trap() {
 // sepc: the pc when fault happens;
 // stval: the virtual address that causes pagefault when being accessed.
 //
-void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
+void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval)
+{
   sprint("handle_page_fault: %lx\n", stval);
-  switch (mcause) {
+  uint64 pa;
+  switch (mcause)
+  {
   case CAUSE_STORE_PAGE_FAULT:
     // TODO (lab2_3): implement the operations that solve the page fault to
     // dynamically increase application stack.
-    // hint: first allocate a new physical page, and then, maps the new page to
-    // the virtual address that causes the page fault. void* pa = alloc_page();
-    // uint64 va = g_ufree_page;
-    // stval += PGSIZE;
-    // user_vm_map((pagetable_t)current->pagetable, stval, PGSIZE, (uint64)pa,
-    //       prot_to_type(PROT_WRITE | PROT_READ, 1));
-    // void* pa = alloc_page();
-    // uint64 va = stval;
-    // USER_STACK_TOP += PGSIZE;
-    // g_ufree_page += PGSIZE;
-    user_vm_map((pagetable_t)current->pagetable, ROUNDDOWN(stval, PGSIZE),
-                PGSIZE, (uint64)(alloc_page()),
-                prot_to_type(PROT_WRITE | PROT_READ, 1));
-    // uint64 va = stval;
-    // stval += PGSIZE;
-    // user_vm_map((pagetable_t)proc->pagetable, USER_STACK_TOP - PGSIZE,
-    // PGSIZE, user_stack,
-    //  prot_to_type(PROT_WRITE | PROT_READ, 1));
-    // USER_STACK_TOP - PGSIZE
-    // user_vm_map((pagetable_t)current->pagetable, stval, PGSIZE, (uint64)pa,
-    // prot_to_type(PROT_WRITE | PROT_READ, 0)); panic( "You need to implement
-    // the operations that actually handle the page fault in lab2_3.\n" );
+    // hint: first allocate a new physical page, and then, maps the new page to the
+    // virtual address that causes the page fault.
+    // panic("You need to implement the operations that actually handle the page fault in lab2_3.\n");
+    pa = (uint64)alloc_page(); // allocate a new physical page
+    if ((void *)pa == NULL)
+      panic("Can not allocate a new physical page.\n");
+    map_pages(current->pagetable, ROUNDDOWN(stval, PGSIZE), PGSIZE, pa, prot_to_type(PROT_READ | PROT_WRITE, 1)); // maps the new page to the virtual address that causes the page fault
     break;
   default:
     sprint("unknown page fault.\n");
@@ -101,28 +78,32 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
 //
 // implements round-robin scheduling. added @lab3_3
 //
-void rrsched() {
+void rrsched()
+{
   // TODO (lab3_3): implements round-robin scheduling.
-  if (current->tick_count + 1 >= TIME_SLICE_LEN) {
-    insert_to_ready_queue(current);
+  // hint: increase the tick_count member of current process by one, if it is bigger than
+  // TIME_SLICE_LEN (means it has consumed its time slice), change its status into READY,
+  // place it in the rear of ready queue, and finally schedule next process to run.
+  // panic("You need to further implement the timer handling in lab3_3.\n");
+  if ((current->tick_count + 1) >= TIME_SLICE_LEN)
+  {
     current->tick_count = 0;
+    current->status = READY;
+    insert_to_ready_queue(current);
     schedule();
-  } else {
-    current->tick_count++;
-    return;
   }
-  // hint: increase the tick_count member of current process by one, if it is
-  // bigger than TIME_SLICE_LEN (means it has consumed its time slice), change
-  // its status into READY, place it in the rear of ready queue, and finally
-  // schedule next process to run. panic( "You need to further implement the
-  // timer handling in lab3_3.\n" );
+  else
+  {
+    current->tick_count++;
+  }
 }
 
 //
-// kernel/smode_trap.S will pass control to smode_trap_handler, when a trap
-// happens in S-mode.
+// kernel/smode_trap.S will pass control to smode_trap_handler, when a trap happens
+// in S-mode.
 //
-void smode_trap_handler(void) {
+void smode_trap_handler(void)
+{
   // make sure we are in User mode before entering the trap handling.
   // we will consider other previous case in lab1_3 (interrupt).
   if ((read_csr(sstatus) & SSTATUS_SPP) != 0)
@@ -137,7 +118,8 @@ void smode_trap_handler(void) {
   uint64 cause = read_csr(scause);
 
   // use switch-case instead of if-else, as there are many cases since lab2_3.
-  switch (cause) {
+  switch (cause)
+  {
   case CAUSE_USER_ECALL:
     handle_syscall(current->trapframe);
     break;
